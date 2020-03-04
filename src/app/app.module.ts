06a48655f7789c66cd0d94c9a0f7517a54e19fdd
hebzebba <kwashieseth34@gmail.com>
2020-03-04 10:44:44 +0000

    Add some ui materials and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SingleblogComponent } from './singleblog/singleblog.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const  appRoutes: Routes = [
+  {path: '', component: LandingpageComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingpageComponent,
+    SingleblogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
